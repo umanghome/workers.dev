b2acffd499174f3473516d14f67e9c2fd764a2be
Umang Galaiya <umanghome@gmail.com>
2020-05-17 16:00:53 +0530

    Add cors worker

---
+++ packages/cors/index.js
@@ -0,0 +1,71 @@
+// We support the GET, POST, HEAD, and OPTIONS methods from any origin,
+// and accept the Content-Type header on requests. These headers must be
+// present on all responses to all CORS requests. In practice, this means
+// all responses to OPTIONS requests.
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
+
+async function handleCORS(request) {
+  const url = new URL(request.url);
+  const apiurl = url.searchParams.get('url'); // This does decodeURIComponent internally
+
+  // Rewrite request to point to API url. This also makes the request mutable
+  // so we can add the correct Origin header to make the API server think
+  // that this request isn't cross-site.
+  request = new Request(apiurl, request);
+
+  request.headers.set('Origin', new URL(apiurl).origin);
+
+  let response = await fetch(request);
+
+  // Recreate the response so we can modify the headers
+  response = new Response(response.body, response);
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', url.origin);
+
+  // Append to/Add Vary header so browser will cache response correctly
+  response.headers.append('Vary', 'Origin');
+
+  return response;
+}
+
+function handleOptions(request) {
+  // Make sure the necesssary headers are present
+  // for this to be a valid pre-flight request
+  if (
+    request.headers.get('Origin') !== null &&
+    request.headers.get('Access-Control-Request-Method') !== null &&
+    request.headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check the requested method + headers
+    // you can do that here.
+    return new Response(null, {
+      headers: corsHeaders,
+    });
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS, PUT, DELETE, PATCH',
+      },
+    });
+  }
+}
+
+addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  if (request.method === 'OPTIONS') {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(request));
+  } else {
+    // Handle requests to the API server
+    event.respondWith(handleCORS(request));
+  }
+});
